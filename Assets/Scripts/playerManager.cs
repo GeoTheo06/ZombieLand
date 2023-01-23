4f09a6e807175332dff12c3e42e11ccbbdbe26de
GeoTheo06 <theodorgeor@gmail.com>
2023-01-23 23:53:15 +0200

    groundCheck and ceilingCheck's coordinates are now relative to the character controller and not fixed numbers as they used to be. Jumping is now fucked up. fix it

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -1,43 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class playerManager : MonoBehaviour
 {
 	GameObject player, gameManager, camera1;
+
 	gameManager1 gameManagerScript;
 	playerMovement playerMovementScript;
 	cameraMovement cameraMovementScript;
 	public int playerHealth;
 	public bool playerDead = false;
+	GameObject groundCheck, ceilingCheck;
+	CharacterController characterController;
 
 	public Animator cameraDie;
+	Vector3 startOfCharacterController, endOfCharacterController;
 
 	private void Start()
 	{
 		player = GameObject.Find("player1");
-		gameManager = GameObject.Find("gameManager");
-		camera1 = GameObject.Find("camera1");
-		playerHealth = 1000;
+@ -24,38 + 22,22 @@ public class playerManager : MonoBehaviour
 		playerMovementScript = player.GetComponent<playerMovement>();
 		gameManagerScript = gameManager.GetComponent<gameManager1>();
 		cameraMovementScript = camera1.GetComponent<cameraMovement>();
+		groundCheck = GameObject.Find("groundCheck");
+		ceilingCheck = GameObject.Find("ceilingCheck");
+		characterController = GameObject.Find("player1").GetComponent<CharacterController>();
 	}
 
-	private void Update()
+private void Update()
+{
+	if (groundCheck.transform.position != startOfCharacterController || ceilingCheck.transform.position != endOfCharacterController)
+	{
+		set_groundCheck_ceilingCheck_position();
+	}
+	playerDies();
+}
+
+void playerDies()
+{
+	if (playerHealth <= 0)
 	{
 		playerDies();
+		playerMovementScript.playerDying = true; // starting animation for dying
+		cameraMovementScript.enabled = false; //disabling camera control
+		gameManagerScript.gameOver = true; //telling the game manager that the game has finished
+		cameraDie.SetBool("isDying", true);
+		playerDead = true;
 	}
-
-	void playerDies()
-	{
-		if (playerHealth <= 0)
-		{
-			playerMovementScript.playerDying = true; // starting animation for dying
-			cameraMovementScript.enabled = false; //disabling camera control
-			gameManagerScript.gameOver = true; //telling the game manager that the game has finished
-			cameraDie.SetBool("isDying", true);
-			playerDead = true;
-		}
-	}
+}
+void set_groundCheck_ceilingCheck_position()
+{
+	//set groundCheck, ceilingCheck position
+	startOfCharacterController = characterController.transform.position;
+	startOfCharacterController = new Vector3(characterController.transform.position.x, 0, characterController.transform.position.z);
+	endOfCharacterController = new Vector3(characterController.transform.position.x, characterController.center.y + characterController.height, characterController.transform.position.z);
+	groundCheck.transform.position = startOfCharacterController;
+	ceilingCheck.transform.position = endOfCharacterController;
+}
+void playerDies()
+{
+	playerMovementScript.playerDying = true; // starting animation for dying
+	cameraMovementScript.enabled = false; //disabling camera control
+	gameManagerScript.gameOver = true; //telling the game manager that the game has finished
+	cameraDie.SetBool("isDying", true);
+	playerDead = true;
 }
